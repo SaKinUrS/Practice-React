4869f51cb47743d1c2c2566f8fd9fa4415c12c14
unknown <sakinurs@mail.ru>
2022-12-10 13:54:36 +0600

    Add to Cart

--- src/App.js
+++ src/App.js
@@ -1,39 +1,72 @@
+import { useState, useEffect } from "react";
 import Card from "./components/Card";
 import Cart from "./components/Cart";
 import Header from "./components/Header";
 
-const arrSneakers = [
-	{ name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, img: 'img/sneakers/03.png' },
-	{ name: 'Мужские Кроссовки Nike Air Max 270', price: 8499, img: '/img/sneakers/02.png' },
-	{ name: 'Мужские Кроссовки Under Armour Curry 8', price: 18499, img: '/img/sneakers/06.png' },
-	{ name: 'Мужские Кроссовки Nike Kyrie 7', price: 45000, img: '/img/sneakers/04.png' },
-]
+
 
 function App() {
+	const [items, setItems] = useState([])
+	const [cartItems, setCartItems] = useState([])
+	const [cartOpened, setCartOpened] = useState(false)
+	const [searchValue, setSearchValue] = useState('')
+
+
+	useEffect(() => {
+		fetch('https://639373a7ab513e12c50ee7dc.mockapi.io/items').then(res => {
+			return res.json()
+		}).then(json => {
+			setItems(json)
+		})
+	}, []);
+
+	const addItemToCart = (obj) => {
+		setCartItems(prev => [...prev, obj])
+	}
+
+	const onChangeSearch = (event) => {
+		setSearchValue(event.target.value)
+	}
+
+
 	return (
 		<div className="wrapper">
-			<Cart />
-			<Header />
-			<section className="catalog">
+			{cartOpened && < Cart items={cartItems} closeCart={() => { setCartOpened(false) }} />}
+			<Header onClickCart={() => setCartOpened(true)} />
+			<div section className="catalog">
 				<div className="catalog__container">
 					<div className="catalog__header">
-						<h3 className="catalog__title">Все кроссовки</h3>
+						<h3 className="catalog__title">{searchValue ? `Поиск по шаблону: "${searchValue}"` : 'Все кроссовки'}</h3>
 						<div className="search">
 							<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round" />
+								<path d="M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z" stroke="#E4E4E4" strokeWidth="2" strokeLinecap="round" />
 							</svg>
-							<input type="text" placeholder="Поиск..." />
+							<input onChange={onChangeSearch} value={searchValue} type="text" placeholder="Поиск..." />
+							{searchValue && <button onClick={() => setSearchValue('')} className="item-cart__remove button-action remove-input">
+								<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3" />
+								</svg>
+							</button>}
 						</div>
 					</div>
 					<div className="catalog__body">
-						{arrSneakers.map(sneaker => (
-							<Card title={sneaker.name} price={sneaker.price} img={sneaker.img} />
+						{items
+							.filter(item => item.name.toLowerCase().includes(searchValue.toLowerCase()))
+							.map(sneaker => (
+								<Card
+									key={sneaker.title}
+									title={sneaker.name}
+									price={sneaker.price}
+									img={sneaker.img}
+									onClickPlus={addItemToCart}
+									onClickFavourite={(obj) => addItemToCart(obj)} />
+							))
 
-						))}
+						}
 
 					</div>
 				</div>
-			</section>
+			</div>
 		</div >
 	);
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,60 +1,36 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({ closeCart, items = [] }) => {
 	return (
-		<div className="overlay" style={{ display: 'none' }}>
+		<div className="overlay">
 			<div className="cart">
 				<h3 className="cart__title">
 					Корзина
-					<button className="item-cart__remove button-action">
+					<button className="item-cart__remove button-action" onClick={closeCart}>
 						<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3" />
 						</svg>
 					</button>
 				</h3>
 				<div className="cart__body">
-					<div className="cart__item item-cart">
-						<div className="item-cart__img">
-							<img src="./img/sneakers/02.png" alt="" />
+					{items.map((item) => (
+						<div className="cart__item item-cart">
+							<div className="item-cart__img">
+								<img src={item.img} alt={item.title} />
+							</div>
+							<div className="item-cart__content">
+								<p className="item-cart__name">{item.title}</p>
+								<span className="item-cart__price">{item.price} руб.</span>
+							</div>
+							<button className="item-cart__remove button-action">
+								<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3" />
+								</svg>
+							</button>
 						</div>
-						<div className="item-cart__content">
-							<p className="item-cart__name">Мужские Кроссовки Nike Air Max 270</p>
-							<span className="item-cart__price">12 999 руб.</span>
-						</div>
-						<button className="item-cart__remove button-action">
-							<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3" />
-							</svg>
-						</button>
-					</div>
-					<div className="cart__item item-cart">
-						<div className="item-cart__img">
-							<img src="./img/sneakers/01.png" alt="" />
-						</div>
-						<div className="item-cart__content">
-							<p className="item-cart__name">Мужские Кроссовки Nike Air Max 270</p>
-							<span className="item-cart__price">8 599 руб.</span>
-						</div>
-						<button className="item-cart__remove button-action">
-							<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3" />
-							</svg>
-						</button>
-					</div>
-					<div className="cart__item item-cart">
-						<div className="item-cart__img">
-							<img src="./img/sneakers/04.png" alt="" />
-						</div>
-						<div className="item-cart__content">
-							<p className="item-cart__name">Мужские Кроссовки Nike Air Max 270</p>
-							<span className="item-cart__price">10 599 руб.</span>
-						</div>
-						<button className="item-cart__remove button-action">
-							<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3" />
-							</svg>
-						</button>
-					</div>
+					))}
+
+
 
 				</div>
 				<div className="total-block">
@@ -73,8 +49,8 @@ const Cart = () => {
 					<button className="total__order green-button">
 						Оформить заказ
 						<svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M1 7H14.7143" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-							<path d="M8.71436 1L14.7144 7L8.71436 13" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M1 7H14.7143" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+							<path d="M8.71436 1L14.7144 7L8.71436 13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
 						</svg>
 
 					</button>
